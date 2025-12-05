511c672e05ca0231dd439b934e0a78f1d3000d2f
João Brentano <joaobrentano.br@gmail.com>
2025-12-05 07:45:46 -0300

    Integração Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
 import 'screens/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const ProjetoASApp());
 }
 
